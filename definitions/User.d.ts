3c7fea748945ebe0a77db8c9e1b9e4d9d4fae012
Adam Berg <adam@xyzdigital.com>
2017-09-14 17:26:55 -0700

    Update User definiton

--- definitions/User.d.ts
+++ definitions/User.d.ts
@@ -1,9 +1,10 @@
+/// <reference types="mongoose" />
 import * as mongoose from 'mongoose';
-export declare const UserSchema: any;
+export declare const UserSchema: mongoose.Schema;
 export interface IUser {
     email?: string;
     password?: string;
 }
 export interface IUserDocument extends IUser, mongoose.Document {
 }
-export declare const User: any;
+export declare const User: mongoose.Model<IUserDocument>;
